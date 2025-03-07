c5883f4c42f7bfa27fbb809e995ccb51472ff0f1
ysauru <artioma981@gmail.com>
2025-03-08 00:15:04 +0400

    commit

--- day 05/home work/main4.py
+++ day 05/home work/main4.py
@@ -1,4 +1,4 @@
-celsius = print(input("Please enter a number in celsius"))
+celsius = print(input("Please enter the temperature in celsius"))
 fanherheit = celsius * 9 / 5 + 32
 fanherheit = round(fanherheit)
 
