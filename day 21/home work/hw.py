92cb58d40915cd37bd0e409ea250d5d3ee640bb0
ysauru <artioma981@gmail.com>
2025-04-12 19:17:33 +0400

    commit

---
+++ day 21/home work/hw.py
@@ -0,0 +1,63 @@
+# 1)https://www.codewars.com/kata/57faece99610ced690000165
+
+# def remove(st):
+#   return st.rstrip("!")
+
+# 2)https://www.codewars.com/kata/57fb09ef2b5314a8a90001ed
+
+# def replace_exclamation(st):
+#    vowels = "aeiouAEIOU"
+#   for vowel in vowels:
+#        st = st.replace(vowel, "!")
+#    return st
+
+# 3) https://www.codewars.com/kata/59dd3ccdded72fc78b000b25
+
+# def whatday(num):
+#    if num == 1:
+#       return "Sunday"
+#    elif num == 2:
+#        return "Monday"
+#    elif num == 3:
+#        return "Tuesday"
+#    elif num == 4:
+#        return "Wednesday"
+#    elif num == 5:
+#        return "Thursday"
+#    elif num == 6:
+#        return "Friday"
+#    elif num == 7:
+#        return "Saturday"
+#    else:
+#       return "Wrong, please enter a number between 1 and 7"
+
+# 4) https://www.codewars.com/kata/57ec8bd8f670e9a47a000f89
+
+
+#  def mouth_size(animal): 
+#    if animal.lower() == "alligator":
+#        return "small"
+#    else:
+#       return "wide"
+
+
+#  5) https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+
+#   def who_is_paying(name):
+#    if len(name) <= 2:
+#       return [name]
+#   else:
+#       return [name, name[:2]]
+
+#  6) https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d/train/python
+
+# def solution(text, ending):
+#     return text.endswith(ending)
+
+# 7) https://www.codewars.com/kata/58712dfa5c538b6fc7000569/train/python
+
+# def count_red_beads(n):
+#    if n <= 2:
+#       return 0
+#   else:
+#       return (n - 1) * 2
