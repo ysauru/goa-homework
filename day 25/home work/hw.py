48fe0978e0967da76ab6a4b8abd7943de2f24e9d
ysauru <artioma981@gmail.com>
2025-04-22 21:26:33 +0400

    commit

---
+++ day 25/home work/hw.py
@@ -0,0 +1,3 @@
+# yvela elements aqvs tavisi id romelic mxolod mas miekutvneba da aris unikaluri zustad magis nairi id sxvas arcerts ar eqneba
+
+# local hostze gashvebisas marto shen shegizlia naxo saiti xolo hosted website is naxva sheuzlia yvelas
